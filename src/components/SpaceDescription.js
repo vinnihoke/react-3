2e4e130905feab5e43950ef3a36bb3d707d73b6a
Vinni Hoke <vinni.hoke@gmail.com>
2019-08-01 20:33:22 -0700

    Converted MVP to Material-UI

--- src/components/SpaceDescription.js
+++ src/components/SpaceDescription.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function SpaceDescription({ imageData }){
 	return(
-		<div className="hoverWindow">
+		<div className="DescriptionWindow">
 			<h3 className="title">{imageData.title}</h3>
 			<h4 className="information"><span>{imageData.copyright}</span> | <span>{imageData.date}</span></h4>
 			<p className="explanation">{imageData.explanation}</p>
